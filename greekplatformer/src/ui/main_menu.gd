52e2b767814e84d0b102cf9498dece82c090c16d
Jan Badel <jan.badel@fer.unizg.hr>
2024-10-26 12:36:45 +0200

    Added Main menu v1.0
    - dodan novi izgled main menua

--- greekplatformer/src/ui/main_menu.gd
+++ greekplatformer/src/ui/main_menu.gd
@@ -9,21 +9,20 @@ func open_menu(option_name:String):
 	get_node("options").get_node(option_name).set_visible(true)
 	get_node("options").get_node(option_name).visible=true
 
-func _on_quit_pressed() -> void:
-	get_tree().quit()
-
-func _on_settings_pressed() -> void:
-	open_menu("settings")
-
-func _on_back_to_main_pressed() -> void:
-	open_menu("main")
-
-func _on_play_pressed() -> void:
-	open_menu("playOptions")
-
-
 func _on_new_game_pressed() -> void:
 	SceneLoader.load_scene(new_game)
 
 func _on_continue_pressed() -> void: # continue the saved file
 	pass # Replace with function body.
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_play_pressed() -> void:
+	open_menu("playOptions")
+
+func _on_settings_pressed() -> void:
+	open_menu("settings")
+	
+func _on_back_to_main_pressed() -> void:
+	open_menu("main")
