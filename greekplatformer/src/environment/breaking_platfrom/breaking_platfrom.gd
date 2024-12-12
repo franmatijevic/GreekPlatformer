65d2767641945f9e1cc71f17455a0c7d86e185a5
Jan Badel <jan.badel@fer.unizg.hr>
2024-12-12 20:36:00 +0100

    Updated breaking platform
    - novi asseti, animacije i particle efekti za breaking platform

--- greekplatformer/src/environment/breaking_platfrom/breaking_platfrom.gd
+++ greekplatformer/src/environment/breaking_platfrom/breaking_platfrom.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 func _on_detect_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 			$AnimationPlayer.play("broken")
+			$CPUParticles2D.emitting = true
 			await $AnimationPlayer.animation_finished
 			queue_free()
 	else:
