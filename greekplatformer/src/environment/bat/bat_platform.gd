1116899afaca06eb17b055d0b9aef4286e34f643
irmazecevic <irmazecevic0@gmail.com>
2025-01-17 22:45:35 +0100

    Sismis platforma

---
+++ greekplatformer/src/environment/bat/bat_platform.gd
@@ -0,0 +1,23 @@
+extends AnimatableBody2D
+
+class_name Bat
+
+@onready var pathFollow = $".."
+
+@export var speed: float = 0.5
+
+var previous_position: Vector2
+
+func _physics_process(delta):
+	
+	previous_position = pathFollow.global_position
+	
+	pathFollow.progress_ratio += speed * delta
+	if pathFollow.progress_ratio == 1:
+		pathFollow.progress_ratio = 0
+	
+	
+	if global_position.x < previous_position.x:
+		get_node("BirdSprite").scale.x = -abs(get_node("BatSprite").scale.x)
+	else:
+		get_node("BirdSprite").scale.x = abs(get_node("BatSprite").scale.x)
