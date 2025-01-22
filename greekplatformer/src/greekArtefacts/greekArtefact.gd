a5d1b4dacebd8375b2aa4bc35adae91cd2c8b70f
Jan Badel <jan.badel@fer.unizg.hr>
2025-01-22 09:20:00 +0100

    Random fixes
    - pausable humans
    - reconfigured UI za artefacte

--- greekplatformer/src/greekArtefacts/greekArtefact.gd
+++ greekplatformer/src/greekArtefacts/greekArtefact.gd
@@ -7,7 +7,7 @@ extends Node2D
 var object_reached = false
 
 func _process(delta: float) -> void:
-	if (object_reached && Input.is_action_pressed("interact")):
+	if (object_reached && Input.is_action_pressed("artefact_interact")):
 		interact.visible = false
 		artefact_information.visible = true
 		SignalBus.emit_on_interacted_artefact()
